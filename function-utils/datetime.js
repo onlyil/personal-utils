84d7979e7060dd5d8afa495a2e0dccc941dd0c11
onlyil <lin.cao@beibei.com>
2019-01-16 14:10:18 +0800

    :sparkles: Add format date fun.

--- function-utils/datetime.js
+++ function-utils/datetime.js
@@ -87,6 +87,38 @@ function getLastDate(date) {
   return getFormatDate(lastDateObj)
 }
 
+/**
+ * get formated date string
+ *
+ * @param {*} date date obj
+ * @param {*} fmt format string
+ * @returns formated date string
+ */
+function formatDate (date, fmt) {
+  if (/(y+)/.test(fmt)) {
+    fmt = fmt.replace(RegExp.$1, (date.getFullYear() + '').substr(4 - RegExp.$1.length))
+  }
+
+  let oReg = {
+    'M+': date.getMonth() + 1,
+    'd+': date.getDate(),
+    'h+': date.getHours(),
+    'm+': date.getMinutes(),
+    's+': date.getSeconds()
+  }
+  for (let key in oReg) {
+    if (new RegExp(`(${key})`).test(fmt)) {
+      let str = oReg[key] + ''
+      fmt = fmt.replace(RegExp.$1, (RegExp.$1.length === 1) ? str : addZero(str))
+    }
+  }
+  return fmt
+}
+
+function addZero (str) {
+  return ('00' + str).substr(str.length)
+}
+
 export default {
   formatDay,
   formatMonth,
@@ -94,5 +126,6 @@ export default {
   getTheMonthDaysNum,
   getWeekOfDate,
   getNextDate,
-  getLastDate
+  getLastDate,
+  formatDate,
 }
